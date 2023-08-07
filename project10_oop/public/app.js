5d96c4083251ac3c05a749f16ad41bd25b275c8e
Ahmed Raza <ahmedraza6100@gmail.com>
2023-08-07 15:56:16 +0500

    project10_oop Completed

---
+++ project10_oop/public/app.js
@@ -0,0 +1,28 @@
+import { Student } from "./student.js";
+import inquirer from "inquirer";
+const { number } = await inquirer.prompt([
+    {
+        type: "number",
+        name: "number",
+        message: "Type 1 if you like to talk to others and 2 if you would rather keep it to yourself: ",
+    },
+]);
+const NewStudent = new Student();
+console.log("1", NewStudent);
+NewStudent.AskQuestion(number);
+console.log("2", NewStudent);
+console.log(`You are: ${NewStudent.GetPersonality()}`);
+const { name } = await inquirer.prompt([
+    {
+        name: "name",
+        message: "Enter Your Name: ",
+    },
+]);
+if (NewStudent.Name === "") {
+    NewStudent.Name = "Unknown";
+}
+else {
+    NewStudent.Name = name;
+}
+console.log("3", NewStudent);
+console.log(`Your Name is ${NewStudent.Name} and your personality is ${NewStudent.GetPersonality()} `);
---
+++ project10_oop/public/person.ts
@@ -0,0 +1,18 @@
+export class Person {
+  private _personality!: string;
+  constructor() {
+    this._personality = "Mystery";
+  }
+  AskQuestion(answer: number) {
+    if (answer === 1) {
+      this._personality = "Extrovert";
+    } else if (answer === 2) {
+      this._personality = "Introvert";
+    } else {
+      this._personality = "Unknown";
+    }
+  }
+  GetPersonality() {
+    return this._personality;
+  }
+}
